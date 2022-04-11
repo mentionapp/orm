63772e4064199f216f71a500e890252f11c802d7
Pierre Tondereau <pierre.tondereau@protonmail.com>
2022-04-11 10:28:18 +0200

    Enhance connection closed by EM stracktrace.

--- src/Exception/EntityManagerClosed.php
+++ src/Exception/EntityManagerClosed.php
@@ -4,10 +4,38 @@ declare(strict_types=1);
 
 namespace Doctrine\ORM\Exception;
 
+use Throwable;
+
 final class EntityManagerClosed extends ORMException implements ManagerException
 {
     public static function create(): self
     {
         return new self('The EntityManager is closed.');
+    }
+
+    public static function createWhere(?Throwable $e = null): self
+    {
+        if ($e === null) {
+            return self::create();
+        }
+
+        return new self(
+            'The EntityManager is closed (previous allows to trace back to where it was closed).',
+            0,
+            $e
+        );
+    }
+
+    public static function createClosedHere(?Throwable $cause = null): self
+    {
+        if ($cause === null) {
+            return new self('This exception allows to trace back to where close() was called.');
+        }
+
+        return new self(
+            'This exception allows to trace back to where close() was called (see previous exception for the cause).',
+            0,
+            $cause
+        );
     }
 }
